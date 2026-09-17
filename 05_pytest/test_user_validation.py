53853ab2d37cb6bc257fb6028ca664613a01b2f9
keerthikrish2019 <keerthikrish2019@gmail.com>
2026-09-17 15:02:09 -0700

    Update test_user_validation.py

--- 05_pytest/test_user_validation.py
+++ 05_pytest/test_user_validation.py
@@ -1,1 +1,19 @@
 
+from sys import path
+
+path.append("01_python_fundamentals")
+
+from user_validation import get_eligible_users
+
+
+def test_get_eligible_users():
+    users = [
+        {"name": "John", "age": 35, "active": True},
+        {"name": "Mary", "age": 25, "active": True},
+        {"name": "Sam", "age": 40, "active": False},
+        {"name": "Priya", "age": 32, "active": True},
+    ]
+
+    result = get_eligible_users(users)
+
+    assert result == ["John", "Priya"]
